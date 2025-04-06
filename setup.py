697b3b5dc3152710dfccf1a39d901a796067f9f5
oscar <oscar@wahltinez.org>
2025-04-06 18:56:41 +1000

    Add setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+# Read the requirements from requirements.txt
+with open("requirements.txt") as f:
+  requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="qsota",
+    version="0.1.0",
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "qsota = qsota:cli",
+        ],
+    },
+)
